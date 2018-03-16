d85d8ca32869ecb56f89c0b3352ec8b5c1dcaba6
Tomsiik <37449127+Tomsiik@users.noreply.github.com>
2018-03-16 18:19:42 +0100

    změna umístěná startupu a přidání configu pro debug a run

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
-
-
 #include "main.h"
 #include "Periph_Init.h"
-
-
+#include "stm32l4xx_ll_gpio.h"
+#include "stm32l4xx_ll_bus.h"
 
 int main(void) {
 	SystemClock_Config();
 
-	while (1) {
 
+	while (1) {
 
 	}
 }
